11b576378bd1e355dd767027ebe19e813db8e468
Ivan 'lostpointer' Khokhlov <lostpointer@gmail.com>
2020-05-18 01:48:44 +0300

    add token

--- secdist.py
+++ secdist.py
@@ -1,6 +1,6 @@
 class SecDist(dict):
     def __init__(self):
-        self['bot']['token'] = ''
+        self['bot']['token'] = '1173660514:AAErTBQxrs-A2OEvmAPcI7UkI8fr_v0rpf4'
 
 
 secdist = SecDist()
