77fcc7f0021beca3edcb79fdb248405df8bf99a1
Ivan 'lostpointer' Khokhlov <lostpointer@gmail.com>
2020-05-18 01:45:56 +0300

    feat bot: add simple bot
    Relates: LEARN-4

---
+++ main.py
@@ -0,0 +1,33 @@
+import telegram
+
+import secdist
+
+
+def main():
+    bot = telegram.Bot(token=secdist.secdist['bot']['token'])
+    while True:
+        try:
+            bot.get_updates(new_offset)
+
+            last_update = bot.get_last_update()
+            print('new message: ', last_update)
+
+            last_update_id = last_update['update_id']
+            last_chat_text = last_update['message']['text']
+            last_chat_id = last_update['message']['chat']['id']
+
+            if last_chat_text.lower() == '/hello':
+                bot.send_message(last_chat_id, 'Hello')
+
+            new_offset = last_update_id + 1
+        except KeyboardInterrupt:
+            exit()
+        except IndexError:
+            pass
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
---
+++ secdist.py
@@ -0,0 +1,6 @@
+class SecDist(dict):
+    def __init__(self):
+        self['bot']['token'] = ''
+
+
+secdist = SecDist()
