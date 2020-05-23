2a104a9175d05c994476b1d4ad8aa14542e751d3
Ivan 'lostpointer' Khokhlov <lostpointer@gmail.com>
2020-05-23 19:20:44 +0300

    remoce secdis

--- secdist.py
+++ secdist.py
@@ -2,7 +2,7 @@ class SecDist:
     def __init__(self):
         self._items = dict()
         self._items['bot'] = {}
-        self._items['bot']['token'] = '1173660514:AAErTBQxrs-A2OEvmAPcI7UkI8fr_v0rpf4'
+        self._items['bot']['token'] = ''
 
     def __getitem__(self, item):
         return self._items[item]
